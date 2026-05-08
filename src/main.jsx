fd4757cc7a739d5d8918dde90ff68c5edab6eb30
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-05-08 16:07:29 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import { ProfileSettingsProvider } from './contexts/ProfileSettingsContext'
-import { GeneralSettingsProvider } from './contexts/GeneralSettingsContext'
-import { BrandProvider } from './contexts/BrandContext'
-import { CustomerProvider } from './contexts/CustomerContext'
-import { OrdersProvider } from './contexts/OrdersContext'
-import { TaskProvider } from './contexts/TaskContext'
-import { InvoiceProvider } from './contexts/InvoiceContext'
-import { PaymentProvider } from './contexts/PaymentContext'
-import { AppointmentProvider } from './contexts/AppointmentContext'
-import { NotificationProvider } from './contexts/NotificationContext'
-import { PremiumProvider } from './contexts/PremiumContext'
-import { GalleryProvider } from './contexts/GalleryContext'
-import { ReviewProvider } from './contexts/ReviewContext'
-import { BodyMeasurementImagesProvider } from './contexts/BodyMeasurementImagesContext'
+import { AuthProvider }                   from './contexts/AuthContext'
+import { ProfileSettingsProvider }        from './contexts/ProfileSettingsContext'
+import { GeneralSettingsProvider }        from './contexts/GeneralSettingsContext'
+import { BrandProvider }                  from './contexts/BrandContext'
+import { CustomerProvider }               from './contexts/CustomerContext'
+import { OrdersProvider }                 from './contexts/OrdersContext'
+import { TaskProvider }                   from './contexts/TaskContext'
+import { InvoiceProvider }                from './contexts/InvoiceContext'
+import { ReceiptProvider }                from './contexts/ReceiptContext'
+import { PaymentProvider }                from './contexts/PaymentContext'
+import { AppointmentProvider }            from './contexts/AppointmentContext'
+import { NotificationProvider }           from './contexts/NotificationContext'
+import { PremiumProvider }                from './contexts/PremiumContext'
+import { GalleryProvider }                from './contexts/GalleryContext'
+import { ReviewProvider }                 from './contexts/ReviewContext'
+import { BodyMeasurementImagesProvider }  from './contexts/BodyMeasurementImagesContext'
 import App from './App'
 import './index.css'
 import { registerSW } from 'virtual:pwa-register'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -36,13 +36,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                         <OrdersProvider>
                           <TaskProvider>
                             <InvoiceProvider>
-                              <PaymentProvider>
-                                <AppointmentProvider>
-                                  <NotificationProvider>
-                                    <App />
-                                  </NotificationProvider>
-                                </AppointmentProvider>
-                              </PaymentProvider>
+                              <ReceiptProvider>
+                                <PaymentProvider>
+                                  <AppointmentProvider>
+                                    <NotificationProvider>
+                                      <App />
+                                    </NotificationProvider>
+                                  </AppointmentProvider>
+                                </PaymentProvider>
+                              </ReceiptProvider>
                             </InvoiceProvider>
                           </TaskProvider>
                         </OrdersProvider>
@@ -53,7 +55,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
               </PremiumProvider>
             </BrandProvider>
           </ProfileSettingsProvider>
-         </GeneralSettingsProvider>
+        </GeneralSettingsProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
